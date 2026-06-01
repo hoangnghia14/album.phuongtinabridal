f5edfe4459ed08a6baeafa16dd407a3aa1c0f1dd
hoangnghia14 <139794577+hoangnghia14@users.noreply.github.com>
2026-06-01 11:45:51 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/album.phuongtinabridal/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
